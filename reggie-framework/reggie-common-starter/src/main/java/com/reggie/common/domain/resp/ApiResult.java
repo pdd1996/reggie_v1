0881471e08591d9bedf1205841dbaad72e65db10
Paddy_Pan <pdd337816464@outlook.com>
2024-08-04 19:43:13 +0800

    增加统一返回结果

---
+++ reggie-framework/reggie-common-starter/src/main/java/com/reggie/common/domain/resp/ApiResult.java
@@ -0,0 +1,37 @@
+package com.reggie.common.domain.resp;
+
+import lombok.Data;
+
+@Data
+public class ApiResult<T> {
+
+    private Boolean success;
+
+    private Integer code;
+
+    private String message;
+
+    private T data;
+
+    public static <T> ApiResult<T> success() {
+        ApiResult<T> result = new ApiResult<>();
+        result.setData(null);
+        result.setSuccess(Boolean.TRUE);
+        return result;
+    }
+
+    public static <T> ApiResult<T> success(T data) {
+        ApiResult<T> result = new ApiResult<>();
+        result.setData(data);
+        result.setSuccess(Boolean.TRUE);
+        return result;
+    }
+
+    public static <T> ApiResult<T> fail(Integer code, String message) {
+        ApiResult<T> result = new ApiResult<>();
+        result.setSuccess(Boolean.FALSE);
+        result.setCode(code);
+        result.setMessage(message);
+        return result;
+    }
+}
