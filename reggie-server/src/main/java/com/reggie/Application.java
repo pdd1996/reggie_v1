89be4716bc16b5e02ae1851bca30fb45aef296ae
Paddy_Pan <pdd337816464@outlook.com>
2024-08-04 11:06:24 +0800

    增加sa-Token

--- reggie-server/src/main/java/com/reggie/Application.java
+++ reggie-server/src/main/java/com/reggie/Application.java
@@ -1,5 +1,7 @@
 package com.reggie;
 
+import cn.dev33.satoken.SaManager;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan(basePackages = "com.reggie.**.mapper")
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
         SpringApplication.run(Application.class,args);
+        System.out.println("启动成功，Sa-Token 配置如下：" + SaManager.getConfig());
     }
 }
